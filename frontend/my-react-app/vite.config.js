fad6cfc47d320b25c64ec306500b2d71d1985137
kirandeep9914 <kinu819599@gmail.com>
2025-02-13 22:33:10 -0800

    Initial commit or Fix missing output.css issue

--- frontend/my-react-app/vite.config.js
+++ frontend/my-react-app/vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from "tailwindcss";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: '.',  // Ensure root is correct
   css: {
     postcss: {
       plugins: [tailwindcss()],
