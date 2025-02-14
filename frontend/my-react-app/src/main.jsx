7ef58ca439e9626a7732296d95788fd116813736
kirandeep9914 <kinu819599@gmail.com>
2025-02-13 21:47:44 -0800

    Initial commit or Fix missing output.css issue

--- frontend/my-react-app/src/main.jsx
+++ frontend/my-react-app/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import "../src/input.css"
 import { Provider } from 'react-redux';
 import {store} from "./redux/store.jsx";
-import "./dist/output.css"
+import "../src/output.css"
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
