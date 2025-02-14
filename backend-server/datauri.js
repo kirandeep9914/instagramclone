feb89021d76c456892b1d3c945f1ae01a900994d
kirandeep9914 <kinu819599@gmail.com>
2025-02-14 03:54:09 -0800

    Removed all files from Git tracking

--- backend-server/datauri.js
+++
@@ -1,9 +0,0 @@
-const DatauriParser = require('datauri/parser');
-const path = require("path")
-const parser = new DatauriParser();
-
-const getdatauri = (file) =>{
-    const extName = path.extname(file.originalname).toString();
-    return parser.format(extName, file.buffer).content;
-}
-module.exports = getdatauri;
--- frontend/my-react-app/backend-server/middleware.js/multer.js
+++
@@ -1,7 +0,0 @@
-const multer = require('multer');
-
-// Set up Multer storage configuration
-const upload = multer({
-   storage:multer.memoryStorage()
-})  // Stores the file in memory as a buffer
-module.exports = upload
